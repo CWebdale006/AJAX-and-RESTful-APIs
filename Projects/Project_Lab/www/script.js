d1f66c24a4a41bbf5a25e6b641fa2a68d1a745c1
connor <cwebdale006@west-mec.org>
2019-09-23 13:08:42 -0700

    setting up files
    starting the 1st project

---
+++ Projects/Project_Lab/www/script.js
@@ -0,0 +1,16 @@
+/*  Project 01_11_02
+
+    Author: Connor J Webdale 
+    Date: 9.20.19 
+
+    Filename: script.js
+*/
+
+"use strict";
+// setting global variables 
+let httpRequest = false;
+let entry = "^IXIC";
+
+function getRequestObject() {
+    
+}

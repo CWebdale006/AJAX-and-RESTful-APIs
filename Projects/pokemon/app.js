41772ef46be89135cafc8605a9665084b2a838bc
connor <cwebdale006@west-mec.org>
2019-10-04 00:48:16 -0700

    looping over card creation
    create multiple cards now, yay!

--- Projects/pokemon/app.js
+++ Projects/pokemon/app.js
@@ -1,27 +1,63 @@
 // getting data 
-fetch('https://pokeapi.co/api/v2/pokemon/gible/')
+for (let i = 1; i < 10; i++) {
+fetch('https://pokeapi.co/api/v2/pokemon/' + i + '/')
     // making sure the response is in json 
     .then(res => res.json())
     .then(
         data => {
+            /*APPENDING INFORMATION AS A DOCUMENT FRAGMENT*/ 
+            // creating the parent div
+            let divCard = document.createElement(`div`);
+            divCard.setAttribute('class', 'card');
+            divCard.setAttribute('id', i);
+            divCard.setAttribute('style', 'width: 30%;');
+
+            // creating the sprite div
+            let divSprite = document.createElement(`div`);
+            divSprite.setAttribute('class', 'sprite');
+            divSprite.setAttribute('style', 'margin: 0 auto');
+            divSprite.setAttribute('id', 'sprite' + i);
+
+            // creating the card-body div
+            let divBody = document.createElement(`div`);
+            divBody.setAttribute('id', 'body' + i);
+            divBody.setAttribute('class', 'card-body');
+
+            // creating the h5 tag
+            let h5Name = document.createElement(`h5`);
+            h5Name.setAttribute('id', 'h5' + i)
+            h5Name.setAttribute('class', 'card-title');
+            h5Name.setAttribute('align', 'center');
+
+            // creating the p tag
+            let pType = document.createElement(`p`);
+            pType.setAttribute('id', 'p' + i)
+            pType.setAttribute('class', 'card-text');
+
+            //putting it all together... 
+            divBody.innerHTML = h5Name.outerHTML + pType.outerHTML;
+            divCard.innerHTML = divSprite.outerHTML + divBody.outerHTML;
+
+            document.body.append(divCard)
+
             // destructuring the data object
             const {...sprites} = data.sprites;
 
-            console.log(data);
+            // console.log(data);
 
             /*ADDING THE SPRITE INTO A DIV*/
-            const htmlSelector = document.getElementById("sprite");
+            const htmlSelector = document.getElementById("sprite" + i);
             const img = `<img src="${sprites.front_shiny}" alt="">`;
             htmlSelector.innerHTML = img;
 
             /*ADDING THE NAME OF POKEMON TO THE DOCUMENT*/ 
-            const htmlName = document.querySelector('h5');
+            const htmlName = document.getElementById('h5' + i);
             const pokemonName = data.name;
             const uppercaseName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
             htmlName.innerHTML = uppercaseName;
 
             /*ADDING THE TYPE OF POKEMON TO DOCUMENT*/
-            const htmlType = document.querySelector('.card-text');
+            const htmlType = document.getElementById('p' + i);
             const typesArray = data.types;
             // checking for single or multiple types 
             if (typesArray.length === 1 ) {
@@ -36,7 +72,7 @@ fetch('https://pokeapi.co/api/v2/pokemon/gible/')
             // sets the last index of the type array to "main type"
             const mainType = typesArray[`${typeIndex}`].type.name;
             // getting the div.card as a constant to affect its styles 
-            const card = document.querySelector('.card')
+            const card = document.getElementById(i);
             if (mainType == 'normal') {
                 card.style.backgroundColor = 'rgba(150, 161, 101, 0.8)';
             } else if (mainType == 'fighting') {
@@ -75,6 +111,8 @@ fetch('https://pokeapi.co/api/v2/pokemon/gible/')
                 card.style.backgroundColor = 'rgba(214, 70, 183, 0.877)';
             } 
 
-            // NEED TO TAKE ALL THIS INFORMATION AND APPEND AS A DOCUMENT FRAGMENT
+            /*ORGANIZING THE POKEMON BY NUMBER */
+            
         }
     );
+    }

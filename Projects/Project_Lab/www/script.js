f52b6b185ab2ea3956f433b11e4c6ada52fe4789
connor <cwebdale006@west-mec.org>
2019-10-29 14:49:19 -0700

    working on a project

--- Projects/Project_Lab/www/script.js
+++ Projects/Project_Lab/www/script.js
@@ -7,22 +7,97 @@
 */
 
 "use strict";
-// setting global variables 
-let httpRequest = false;
-let entry = "^IXIC";
+
+// global variables 
+var httpRequest = false;
+var entry = "^IXIC"; 
 
 function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest();
-    } catch (requestError) {
-        return false; 
     }
-    alert(httpRequest);
-    return
+    catch (requestError) {
+        return false;
+    }
+    return httpRequest;
 }
 
-if (window.addEventListener) {
-    window.addEventListener("load", getRequestObject, false);
-} else if (window.attachEvent) {
-    window.attachEvent("onload", getRequestObject);
+// stops and default submission from executing 
+function stopSubmission(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    }
+    else {
+        evt.returnValue = false;
+    }
+    getQuote();
+}
+
+// requests stock quote data from the server 
+function getQuote() {
+    if (document.getElementsByTagName("input")[0].value) {
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    httpRequest.abort();
+    httpRequest.open("get", "StockCheck.php?t=" + entry, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+clearTimeout(updateQuote);
+var updateQuote = setTimeout('getQuote()', 10000);
+}
+
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var stockResults = httpRequest.responseText;
+        var stockItems = stockResults.split(/,|\"/);
+        for (var i = stockItems.length - 1; i >= 0; i--) {
+            if (stockItems[i] === "") {
+                stockItems.splice(i, 1);
+            }
+        }
+        document.getElementById("ticker").innerHTML = stockItems[0];
+        document.getElementById("openingPrice").innerHTML = 
+            stockItems[6];
+        document.getElementById("lastTrade").innerHTML = 
+            stockItems[1];
+        document.getElementById("lastTradeDT").innerHTML = 
+            stockItems[2] + ", " + stockItems[3];
+        document.getElementById("change").innerHTML = stockItems[4];
+        document.getElementById("range").innerHTML = (stockItems[8] 
+            * 1).toFixed(2) + " &ndash; " + (stockItems[7] * 1).toFixed(2);
+        document.getElementById("volume").innerHTML = (stockItems[9] 
+            * 1).toLocaleString();
+     }
+}
+
+// data in a better style 
+function formatTable() {
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style.background = "#9FE098";
+    }
+}
+
+// event handlers 
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", getQuote, false);
+} 
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", getQuote);
+}
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", formatTable, false);
+    window.addEventListener("load", getQuote, false);
+} 
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", formatTable);
+    window.attachEvent("onload", getQuote);
 }
